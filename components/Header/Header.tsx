fbeca872af02104c6124fc103b7f94a90acf6d30
NguyenThiTuyetBang <bangtuyet2269@gmail.com>
2022-10-01 21:23:51 +0700

    Code header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 
-function Header() {
-  return <div className="sticky bg-[#040714] top-0 z-[10000] flex h-[72px]">
 
+function Header() {
+  return <div className="sticky bg-[#040714] top-0 z-[10000] flex h-[72px] items-center px-10 md:px-12">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8qUu8TCsGrv9KzNhpCB31RlvlcR1kBIh_oOZShuxCzrJhHgZmxswYHNAssFlBdnu9y3Q&usqp=CAU" width={80} height={80} />
   </div>;
 }
 
